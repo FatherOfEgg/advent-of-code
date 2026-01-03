f41c193fdc66bbf881a3da3a5c82d1f25155d3cc
FatherOfEgg <fatherofegg@gmail.com>
2026-01-02 19:29:39 -0500

    Added C 2015 D04 P 1 & 2

---
+++ C/2015/day04/p1.c
@@ -0,0 +1,44 @@
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <openssl/evp.h>
+
+int main() {
+    const char *input = "yzbqklnj";
+    uint64_t num = 0;
+
+    char buf[256];
+
+    unsigned char hash[EVP_MAX_MD_SIZE];
+    unsigned int hashLen;
+
+    EVP_MD_CTX *ctx = EVP_MD_CTX_new();
+    const EVP_MD *type = EVP_md5();
+
+    while (true) {
+        EVP_DigestInit(ctx, type);
+
+        snprintf(buf, sizeof(buf), "%s%lu", input, num);
+
+        EVP_DigestUpdate(ctx, buf, strlen(buf));
+        EVP_DigestFinal(ctx, hash, &hashLen);
+
+        if (hash[0] == 0
+        &&  hash[1] == 0
+        &&  (hash[2] & 0xF0) == 0) {
+            break;
+        }
+
+        num++;
+    }
+
+    EVP_MD_CTX_free(ctx);
+
+    printf("%lu\n", num);
+
+    return EXIT_SUCCESS;
+}
